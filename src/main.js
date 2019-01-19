a810fec2f3664d50f49b2e9d96a846c442c61b42
TinyChief <narsacgm@gmail.com>
2019-01-20 01:23:48 +0300

    service worker and _redirects

--- src/main.js
+++ src/main.js
@@ -1,19 +1,12 @@
 import Vue from 'vue'
 import App from '@/App.vue'
-// import Menu from '@/components/reusable/Menu.vue'
-// import Toolbar from '@/components/reusable/Toolbar.vue'
-// import Icon from '@/components/reusable/Icon.vue'
-// import TaskBlock from '@/components/reusable/TaskBlock.vue'
 import router from '@/router.js'
 import { store } from '@/store/index.js'
 import vueCalendar from 'vue2-simple-calendar'
 import './assets/vue-calendar.css'
 import '@/assets/fontawesome/css/all.css'
 
-// Vue.component('Menu', Menu)
-// Vue.component('Toolbar', Toolbar)
-// Vue.component('Icon', Icon)
-// Vue.component('TaskBlock', TaskBlock)
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 

3c0a3e4e20aca2746ee2d9cd95f958292100b581
TinyChief <narsacgm@gmail.com>
2019-02-06 18:37:23 +0300

    favicon & theme color

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   // ...other vue-cli plugin options...
-  // pwa: {
-  //   name: 'Todo App',
-  //   themeColor: '#b8b8b8',
-  //   msTileColor: '#000000',
-  //   appleMobileWebAppCapable: 'yes',
-  //   appleMobileWebAppStatusBarStyle: 'black',
-  //   iconPaths: {
-  //     favicon32: 'img/icons/favicon-32x32.png',
-  //     favicon16: 'img/icons/favicon-16x16.png'
-  //   }
-  // }
+  pwa: {
+    name: 'Todo App',
+    themeColor: '#939ca5',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png'
+    }
+  }
 }
